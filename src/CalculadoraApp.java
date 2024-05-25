3d02c879c6d332969a0dbb2a9cb5adcb0853f331
Ignacio Velazquez <ignaciov92@gmail.com>
2024-05-25 20:58:29 -0300

    se termina el programa

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -3,8 +3,30 @@ import java.util.Scanner;
 public class CalculadoraApp {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
-        System.out.println("***...:: Aplicación de Calculadora::...***");
+        while (true) {
+            System.out.println("***...:: Aplicación de Calculadora::...***");
+            mostrarMenu();
+            try {
+                var operacion = Integer.parseInt(consola.nextLine());
 
+                if (operacion >= 1 && operacion <= 4) {
+                    ejecutarOperacion(operacion , consola);
+                }
+                else if(operacion ==5) {
+                    System.out.println("Hasta pronto...");
+                    break;
+                }
+                else System.out.println("La opcion no es valida!!!");
+            }
+            catch (Exception e) {
+                System.out.println("Opción ingresada no valida \n" + e.getMessage());
+            }
+
+
+        }
+    }
+
+    private static void mostrarMenu(){
         //mostramos el menu:
         System.out.println("""
                 1. Suma
@@ -15,50 +37,36 @@ public class CalculadoraApp {
                 """);
 
         System.out.print("Digite la operacion que desea realizar: ");
-        var operacion = Integer.parseInt(consola.nextLine());
+    }
 
-        if (operacion >= 1 && operacion <= 4) {
+    private static void ejecutarOperacion(int operacion, Scanner consola){
+        System.out.print("Ingrese el valor del primer operando: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
 
-            System.out.print("Ingrese el valor del primer operando: ");
-            var operando1 = Integer.parseInt(consola.nextLine());
+        System.out.print("Ingrese el valor del segundo operando: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
 
-            System.out.print("Ingrese el valor del segundo operando: ");
-            var operando2 = Integer.parseInt(consola.nextLine());
+        double resultado;
 
-            int resultado;
-
-
-            switch (operacion) {
-                case 1 -> {
-                    resultado = operando1 + operando2;
-                    System.out.println("resultado = " + resultado);
-                }
-                case 2 -> {
-                    resultado = operando1 - operando2;
-                    System.out.println("resultado = " + resultado);
-                }
-                case 3 -> {
-                    resultado = operando1 * operando2;
-                    System.out.println("resultado = " + resultado);
-
-                }
-                case 4->{
-                    resultado = operando1/operando2;
-                    System.out.println("resultado = " + resultado);
-                }
-
-                default -> {
-                    System.out.println("Operacion incorrecta");
-                }
+        switch (operacion) {
+            case 1 -> {
+                resultado = operando1 + operando2;
+                System.out.println("resultado = " + resultado);
+            }
+            case 2 -> {
+                resultado = operando1 - operando2;
+                System.out.println("resultado = " + resultado);
+            }
+            case 3 -> {
+                resultado = operando1 * operando2;
+                System.out.println("resultado = " + resultado);
 
             }
-
-
-        }else{
-            System.out.println("La opcion no es valida!!!");
+            case 4->{
+                resultado = operando1/operando2;
+                System.out.println("resultado = " + resultado);
+            }
         }
-
-
     }
 }
 
